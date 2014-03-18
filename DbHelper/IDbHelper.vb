2ca34ca52ec452dba1a121184aa7cf4bf4d725ee
greatbody <wedown@163.com>
2014-03-18 17:45:12 +0800

    重构数据库访问层的接口
    Signed-off-by: greatbody <wedown@163.com>

--- DbHelper/IDbHelper.vb
+++ DbHelper/IDbHelper.vb
@@ -1,11 +1,15 @@
 ﻿Public Interface IDbHelper
     Function ExcuteNonQuery() As Integer
     Function ExcuteNonQuery(ByVal SqlString As String) As Integer
+
     Function ExecuteScalar(Of T)() As T
-    Function ExecuteScalar(Of T)(ByVal sqlString As String) As T
+    Function ExecuteScalar(Of T)(ByVal sqlComm As OleDb.OleDbCommand) As T
+
     Function FillDataTable() As DataTable
-    Function FillDataTable(ByVal SqlString As String) As DataTable
+    Function FillDataTable(ByVal sqlComm As OleDb.OleDbCommand) As DataTable
+
     Function FillDataSet() As DataSet
-    Function FillDataSet(ByVal SqlString As String) As DataSet
+    Function FillDataSet(ByVal sqlComm As OleDb.OleDbCommand) As DataSet
+
     Function From(ByVal sqlStr As String) As AccessDbHelper
 End Interface
